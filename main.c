4631d7ccedf82dd0970758ca214824b6e8ac0aae
Nicolo <nicoloridulfo@gmail.com>
2025-08-29 20:27:54 +0200

    fix: remove auto-saving because it's buggy

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "editor.h"
 #include "term.h"
-#include <signal.h>
 #include <unistd.h>
 
 #define _POSIX_C_SOURCE 200112L
@@ -8,14 +7,6 @@
 // Good resources
 // https://gist.github.com/delameter/b9772a0bf19032f977b985091f0eb5c1
 // https://vt100.net/annarbor/aaa-ug/section6.html
-
-static void autosave_on_inactive(void) {
-  pt_save_to_file(pt_global_state.filename);
-  pt_move_cursor(1, 1);
-  printf("Autosaved to %s", pt_global_state.filename);
-  pt_move_cursor(2, 1);
-  fflush(stdout);
-}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -30,12 +21,9 @@ int main(int argc, char *argv[]) {
   pt_render_state();
   pt_splash_screen();
 
-  signal(SIGALRM, (void (*)(int))autosave_on_inactive);
-
   while (1) {
     pt_render_state();
     pt_process_key_press();
-    alarm(5);
   }
 
   return 0;
